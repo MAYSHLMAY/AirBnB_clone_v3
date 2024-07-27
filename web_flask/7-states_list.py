003f1d835ead8fd21c1c0b050441eb0763e6d8e5
Mikiyas <educationforlife424@gmail.com>
2024-07-27 14:44:55 +0300

    update some

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-""" this module contains a script that starts a Flask web application
-    the web application must be listening on 0.0.0.0, port 5000
-    Routes: - /states_list """
-from models import *
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+""" Starts a Flask web app """
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 app = Flask(__name__)
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ display HTML page with list of states """
-    states = storage.all(classes["State"]).values()
-    # ^ fetches States data from storage engine, then in line below,
-    # those states are passed into the template
-    return render_template('7-states_list.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def remove_SQLalc_session(exception):
-    """ close storage when tear down is executed """
+def dispose(exception):
+    """ Remove current session """
     storage.close()
+
+
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
